3577ecff88da6ab206dbdfa5b5eaac1bae36cb54
pipopa <pipopa.710@gmail.com>
2020-04-12 11:36:53 +0900

    move:ファイルをsrc配下に移動

--- src/nmado.js
+++ src/nmado.js
@@ -1,5 +1,6 @@
 chrome.runtime.onMessage.addListener((message) => {
   let url = 'https://www.youtube.com/watch?v=' + message.youtubeId
+  console.log(url)
   let input = document.getElementById('video-url')
   input.value = url
   let addButton = document.getElementById('add-video-button')
